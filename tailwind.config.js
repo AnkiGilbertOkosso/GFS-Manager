c585b6fc9ac92f21e4f438c2e4c9814c28916957
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-03-02 06:11:19 +0100

    front end for login page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#1C75BC',
+        'secondary': '#F47827',
+        'orange': '#FF570C'
+      
+      }
+    },
+  },
+  plugins: [],
+}
+
